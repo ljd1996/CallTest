a9fa5f921242b37cd65f2f6f37462112444aab8f
liujiadong <liujiadong@xunlei.com>
2020-01-22 15:21:33 +0800

    VideoView全屏

---
+++ app/src/main/java/com/hearing/calltest/widget/CustomVideoView.java
@@ -0,0 +1,31 @@
+package com.hearing.calltest.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * @author liujiadong
+ * @since 2020/1/22
+ */
+public class CustomVideoView extends VideoView {
+
+    public CustomVideoView(Context context) {
+        super(context);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(0, widthMeasureSpec);
+        int height = getDefaultSize(0, heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+}
